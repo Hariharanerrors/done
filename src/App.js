1173f5407b4186d6f88c6326a2f7309e09cfd9f8
Hariharanerrors <srdhari04@gmail.com>
2023-12-03 19:11:56 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+    const [email,setEmail] = useState('');
+    const [emailError,setEmailError] = useState(false); 
+    const [password,setPassword] = useState('');
+    const [passwordError,setpasswordError] = useState(false); 
+    const [products,setproducts] = useState([]);                
+
+    
+
+    
+
+
+    const handleclick = (event)=> {
+        let cemail = email;
+        let cpassword =password;
+        if(cemail) {
+            setEmailError(false);
+        }else {
+            setEmailError(true);
+
+        }
+        
+        if(cpassword) {
+            setpasswordError(false);
+        }else {
+            setpasswordError(true);
+
+        }
+        
+    }
+    const handleInputChange=(event)=> {
+        let id = event.target.id;
+        let value = event.target.value;
+
+        if(id == "email"){
+            if(value.length>0) {
+                setEmail(value); 
+                setEmailError(false);
+            }
+            else {
+                setEmailError(true);
+            }
+        }
+
+        if(id == "password"){
+            if(value.length>0) {
+                setPassword(value); 
+                setpasswordError(false);
+            }
+            else {
+                setpasswordError(true);
+            }
+        }
+
+        
+
+    }
+
+
+
+
+
+
+    return (
+        <>
+        <div className="regi-sum">
+            <h1 className="regi-form">Registration Form</h1>
+            <form>
+            <div className="emailpass">
+                <span>Email</span>
+                <input type="text" id="email"  value={email} placeholder="Enter your email" className="input" onChange={handleInputChange}  />
+                {emailError ? <label style={{color:"red"}}>Email is not required</label>:""}
+            </div>
+            <div className="emailpass">
+                <span>Password</span>
+                <input type="password" id="password" value={password} placeholder="Enter your password" className="input-2" onChange={handleInputChange} />
+                {passwordError ? <label style={{color:"red"}}>password is not required</label>:""}
+            </div>
+
+            <input type="button" className="btns" onClick={handleclick} value="submit" />
+            </form>
+            
+        </div>
+       
+
+        </>
+        
+       
+    
+    );
 }
+
 
 export default App;
